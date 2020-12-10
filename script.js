8a258d0acbac74f6a46e49e33d5f8920282f331a
Reece Kenney <rekenney@microsoft.com>
2020-12-10 09:33:46 +0000

    Close button + adding font family

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+$(".modal .close").click(function() {
+  // Get the modal that this close button belongs to
+  var modal = $(this).closest(".modal");
+  modal.hide();
+});
+
 var Project1Div = document.getElementById("project1");
 var Project2Div = document.getElementById("project2");
 var Project3Div = document.getElementById("project3");
@@ -5,11 +11,6 @@ var Project4Div = document.getElementById("project4");
 var project1Modal = document.getElementById("project1Modal");
 var project2Modal = document.getElementById("project2Modal");
 var project3Modal = document.getElementById("project3Modal");
-
-
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 Project1Div.onclick = function() {
@@ -20,12 +21,6 @@ Project2Div.onclick = function() {
 }
 Project3Div.onclick = function() {
   project3Modal.style.display = "block";
-}
-
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
